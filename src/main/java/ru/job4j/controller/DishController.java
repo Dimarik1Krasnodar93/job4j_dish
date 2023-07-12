5d6d7ad654fe3fe9b1e5658244f21426b17095f3
Dmitry <krasnodar93@mail.ru>
2023-07-12 13:04:16 +0300

    Обработка исключений в RestTemplate [#505058]

--- src/main/java/ru/job4j/controller/DishController.java
+++ src/main/java/ru/job4j/controller/DishController.java
@@ -1,17 +1,24 @@
 package ru.job4j.controller;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.util.JSONPObject;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import ru.job4j.Exception.IdNotFoundException;
 import ru.job4j.model.Dish;
 
 @RestController
 public class DishController {
     @GetMapping("/order/{id}")
     public Dish getDish(@PathVariable("id")int id) throws JsonProcessingException {
+        return new Dish(1, "test_name", "test_description");
+    }
+
+    @GetMapping("/order/valid/{id}")
+    public Dish getDishValid(@PathVariable("id")int id) throws Exception {
+        if (id <= 0) {
+            throw new IdNotFoundException("id should be positive!");
+        }
         return new Dish(1, "test_name", "test_description");
     }
 }
---
+++ src/main/java/ru/job4j/controllerAdvice/ExceptionApiHandler.java
@@ -0,0 +1,19 @@
+package ru.job4j.controllerAdvice;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import ru.job4j.Exception.IdNotFoundException;
+import ru.job4j.dto.ErrorMessage;
+
+@RestControllerAdvice
+public class ExceptionApiHandler {
+
+    @ExceptionHandler(IdNotFoundException.class)
+    public ResponseEntity<ErrorMessage>  dishException(IdNotFoundException dishException) {
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(new ErrorMessage(dishException.getMessage()));
+    }
+}
